b66730853d713dd2444a5499afc0b40f7e72c7a8
sinuok2e-blip <sinuok2e@gmail.com>
2026-04-02 01:52:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Client({
 
 const TOKEN = process.env.TOKEN;;
 const GUILD_ID = "1222333420810670080";
-const CHANNEL_ID = "1446906108428943400";
+const CHANNEL_ID = "1423656611162886317";
 
 client.once('ready', async () => {
     console.log(`Bot online sebagai ${client.user.tag}`);
